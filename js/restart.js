f28990cbf509f195eed5165dc63497ea5a1e189f
Lex010 <lex07672@gmail.com>
2023-04-19 15:19:46 +0300

    003

---
+++ js/restart.js
@@ -0,0 +1,15 @@
+
+
+const restart = document.getElementById('restart')
+
+restart.onclick = function rest() {
+ 
+ch.textContent = '90%'   
+ench.style.display = 'inline-block'    
+name.textContent = 'Меч +0'
+img.style.backgroundImage = "url('sword/sw0.png')"
+
+
+restart.style.display = 'none'
+
+}
