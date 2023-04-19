72e34bc8b6aee16c836e6c9fb45fd78b5b1bb86b
Lex010 <lex07672@gmail.com>
2023-04-19 11:37:02 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@ const ch = document.getElementById('ch')
 let percent = (Math.random() * 100).toFixed(2)
 
 ench.onclick = function game() {
+if(name.textContent === 'Меч +0' ) {
     if (percent <= 90) {   
 name.textContent = 'Меч +1'
 ch.textContent = '- успех 80% -'
@@ -17,7 +18,26 @@ img.style.backgroundImage = "url('sword/sw1.png')"
         ch.textContent = ''
         img.style.backgroundImage = "url('sword/sw_broken.png')"    
     }
+    return
+} 
+
+if(name.textContent === 'Меч +1' ) {
+    if (percent <= 80) {   
+name.textContent = 'Меч +2'
+ch.textContent = '- успех 75% -'
+img.style.backgroundImage = "url('sword/sw2.png')"
+    } else {
+        ench.style.background = 'red'    
+        ench.textContent = 'Рестар'    
+        name.textContent = 'Все пропало'
+        ch.textContent = ''
+        img.style.backgroundImage = "url('sword/sw_broken.png')"    
+    }
+    return
+} 
+
 }
+
 
 
 
