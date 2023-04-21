6a657177bd0520445121b9731481afa550c749d8
Lex010 <lex07672@gmail.com>
2023-04-21 15:19:20 +0300

    04

---
+++ js/gold_button.js
@@ -0,0 +1,6 @@
+const goltButt = document.getElementById('ench_red')
+const elem = document.getElementsByClassName('ench_red_img_count') 
+
+goltButt.onclick = function() {
+    elem.textContent = 'qwe'
+}
