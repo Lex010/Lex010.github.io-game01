0e34c0dbb2d0294483f91eefc05d414701dcadf7
Lex010 <lex07672@gmail.com>
2023-04-25 14:01:37 +0300

    02

---
+++ js/repear_three_in_row.js
@@ -0,0 +1,50 @@
+buttonReStart.onclick = function() {
+   //document.querySelector( "#" + config.gemIdPrefix + "_" + "0" + "_" + "5" ).classList.add( "remove" );  удаляет указаный гем после сложения 3х гемов
+
+   //document.querySelector("#" + config.gemIdPrefix + "_0_5").classList.add( "remove" );                //тоже самое по другому записаное
+   //gemFade()                                                                                           //gemFade() убирает сразу после нажатия на обновить
+
+  /**/ document.querySelector("#" + config.gemIdPrefix + "_0_0").classList.add( "remove" );  
+   document.querySelector("#" + config.gemIdPrefix + "_0_1").classList.add( "remove" ); 
+   document.querySelector("#" + config.gemIdPrefix + "_0_2").classList.add( "remove" ); 
+   document.querySelector("#" + config.gemIdPrefix + "_0_3").classList.add( "remove" );     ////////выбираю каждый гем по штучно, позже надо заменить на нормальны способ
+   document.querySelector("#" + config.gemIdPrefix + "_0_4").classList.add( "remove" ); 
+   document.querySelector("#" + config.gemIdPrefix + "_0_5").classList.add( "remove" ); 
+
+   document.querySelector("#" + config.gemIdPrefix + "_1_0").classList.add( "remove" ); 
+   document.querySelector("#" + config.gemIdPrefix + "_1_1").classList.add( "remove" ); 
+   document.querySelector("#" + config.gemIdPrefix + "_1_2").classList.add( "remove" ); 
+   document.querySelector("#" + config.gemIdPrefix + "_1_3").classList.add( "remove" ); 
+   document.querySelector("#" + config.gemIdPrefix + "_1_4").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_1_5").classList.add( "remove" ); 
+   
+   document.querySelector("#" + config.gemIdPrefix + "_2_0").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_2_1").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_2_2").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_2_3").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_2_4").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_2_5").classList.add( "remove" );
+
+   document.querySelector("#" + config.gemIdPrefix + "_3_0").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_3_1").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_3_2").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_3_3").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_3_4").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_3_5").classList.add( "remove" );
+
+   document.querySelector("#" + config.gemIdPrefix + "_4_0").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_4_1").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_4_2").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_4_3").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_4_4").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_4_5").classList.add( "remove" );
+
+   document.querySelector("#" + config.gemIdPrefix + "_5_0").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_5_1").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_5_2").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_5_3").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_5_4").classList.add( "remove" );
+   document.querySelector("#" + config.gemIdPrefix + "_5_5").classList.add( "remove" );
+   gemFade()     //удаляет выбраные гемы
+   initGame();  //перезапускает поле
+}
