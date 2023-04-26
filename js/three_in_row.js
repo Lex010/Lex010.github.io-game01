64470ec94e131d7bd17e7f140e6691c5f75b43be
Lex010 <lex07672@gmail.com>
2023-04-26 15:56:46 +0300

    01

--- js/three_in_row.js
+++ js/three_in_row.js
@@ -449,6 +449,10 @@ function removeGems( row, col ) {
 if(config.imagesCoin[ components.gems[row][col] ] === "resorces_im/tempor_6.png"){                                               //добавление ресурсов при сложении пазлов                            
 	resourcesCount[1].textContent = Number(resourcesCount[1].textContent) /**/ + 1 * countRemoveGem; /**/ }                //  добавляет железо  /**/+ 1 * countRemoveGem; /**/ 
 ////////////////////////////////////////////////////                                                /**/+ 1 * countRemoveGem; /**/ убраное количество гемов = добавленные ресурсы
+if(config.imagesCoin[ components.gems[row][col] ] === "resorces_im/tempor_7.png") {
+	resourcesCount[0].textContent = Number(resourcesCount[0].textContent) /**/ + 1 * countRemoveGem; /**/                 //прибавка к дереву  
+}
+///////////////////////////////////////////////////
 	components.gems[ row ][ col ] = -1;
 
 	scoreInc( countRemoveGem );
