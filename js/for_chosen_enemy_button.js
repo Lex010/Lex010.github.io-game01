e7fc10fb0da97e6826d9924160f341cedf53883c
Lex010 <lex07672@gmail.com>
2023-05-02 17:47:36 +0300

    Update for_chosen_enemy_button.js

--- js/for_chosen_enemy_button.js
+++ js/for_chosen_enemy_button.js
@@ -1,28 +1,43 @@
-rightChosenEnButt.onclick = function() {
+rightChosenEnButt.onclick = function() {                         //<=  правая кнопка  =>
    if (enemyName[0].textContent === 'Противник') {
-    enemyName[0].textContent = 'Вепрь'
-    enemyImgBack[0].style.opacity = '0'
-    enemyImg[0].style.opacity = '1'
+    enemyName[0].textContent = 'Вепрь'                    //появляется имя противника
+    healthBar.textContent = 300                          //текущий уровень здоровья противника
+    fixedHealthEnemy.textContent = 300                  //максимальный уровень здоровья(скрытый)
+
+    healthBarBeforStart.style.opacity = '0'             //убирает старую полоску здоровья
+    healthBar.style.opacity = '1'                      //появляется другая полоска здоровья
+
+    enemyImgBack[0].style.opacity = '0'                //убирает старое изображеие противника
+    enemyImg[0].style.opacity = '1'                   //появляется новое изображение противника
     return
    }
    if (enemyName[0].textContent === 'Вепрь') {
     enemyName[0].textContent = 'Леший'
+    healthBar.textContent = 999                          //текущий уровень здоровья противника
+    fixedHealthEnemy.textContent = 999                  //максимальный уровень здоровья(скрытый)
+
+
     enemyImg[0].style.opacity = '0'
     enemyImgSecond[0].style.opacity = '1'
     return
    }
    if (enemyName[0].textContent === 'Леший') {
     enemyName[0].textContent = 'Горыныч Змей'
+    healthBar.textContent = 3000                          //текущий уровень здоровья противника
+    fixedHealthEnemy.textContent = 3000                  //максимальный уровень здоровья(скрытый)
+
     enemyImgSecond[0].style.opacity = '0'
     enemyImgThree[0].style.opacity = '1'
     return
    }
    if (enemyName[0].textContent === 'Горыныч Змей') {
     enemyName[0].textContent = 'Вепрь'
+    healthBar.textContent = 300                          //текущий уровень здоровья противника
+    fixedHealthEnemy.textContent = 300                  //максимальный уровень здоровья(скрытый)
+
     enemyImgThree[0].style.opacity = '0'
     enemyImg[0].style.opacity = '1'
     return
-   }
-   
-  
+   } 
 } 
+///////////////////////////////////////////////////////
